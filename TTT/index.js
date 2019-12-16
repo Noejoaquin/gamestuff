b3703e67fd5ad83cbda04e4f1cb9ca2be5720c9d
Noe <noe.joaquin.james@gmail.com>
2019-12-16 17:03:58 -0500

    add pseudo code for play method

--- TTT/index.js
+++ TTT/index.js
@@ -1,6 +1,7 @@
 class Player {
-  constructor(tileMarker) {
+  constructor(tileMarker, turn) {
     let marker = tileMarker;
+    let turn = turn;
   }
 }
 
@@ -16,8 +17,8 @@ class Board {
 
 class Game {
   constructor() {
-    const player1 = new Player("X");
-    const player2 = new Player("O");
+    const player1 = new Player("X", "One");
+    const player2 = new Player("O", "Two");
     let board = new Board;
     let playerToMove = null;
   }
@@ -25,9 +26,17 @@ class Game {
   playGame() {
     console.log("playing game!!!")
 
-    while (!Board.gameOver()) {
+    while (!board.gameOver()) {
       let playerToPlay = determinePlayerToPlay();
-      document.getElementById('player-to-play').innerHTML = ""
+      document
+        .getElementById('player-to-play')
+        .innerHTML = `Player ${playerToPlay.turn}`
+      board.play(playerToPlay)
+    }
+    if (board.winner) {
+      return board.winner
+    } else {
+      console.log("it's a tie!")
     }
   }
 }
