b78f82fc1412af0004d63db2bdded71640bd51a8
Noe <noe.joaquin.james@gmail.com>
2019-12-16 17:16:27 -0500

    more on the board

--- TTT/index.js
+++ TTT/index.js
@@ -12,6 +12,12 @@ class Board {
       [null, null, null],
       [null, null, null],
     ]
+    let winner = null;
+  }
+
+  play(playerToPlay) {
+    // set up the listeners
+    console.log('play')
   }
 }
 
@@ -20,23 +26,31 @@ class Game {
     const player1 = new Player("X", "One");
     const player2 = new Player("O", "Two");
     let board = new Board;
-    let playerToMove = null;
+    let lastPlayerToMove = null;
   }
 
   playGame() {
     console.log("playing game!!!")
 
     while (!board.gameOver()) {
-      let playerToPlay = determinePlayerToPlay();
+      let playerToPlay = determinePlayerToPlay(lastPlayerToMove);
       document
         .getElementById('player-to-play')
         .innerHTML = `Player ${playerToPlay.turn}`
-      board.play(playerToPlay)
+      board.play(playerToPlay) // this is where we are right now
     }
     if (board.winner) {
       return board.winner
     } else {
       console.log("it's a tie!")
+    }
+  }
+
+  determinePlayerToPlay(lastPlayerToMove) {
+    if(lastPlayerToMove === null || lastPlayerToMove === player2) {
+      return player1
+    } else {
+      return playerToMove
     }
   }
 }
