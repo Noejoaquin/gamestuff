424b995336a2c1fee022c22982cfd79216492dd9
Noe <noe.joaquin.james@gmail.com>
2019-12-18 15:46:58 -0500

    generate board

--- TTT/index.js
+++ TTT/index.js
@@ -1,46 +1,74 @@
 class Player {
   constructor(tileMarker, turn) {
-    let marker = tileMarker;
-    let turn = turn;
+    this.marker = tileMarker;
+    this.turn = turn;
   }
 }
 
 class Board {
-  constructor() {
-    let board = [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null],
-    ]
+  initializeBoard() {
+    let board = document.getElementById("board")
+    for(let i = 0; i < 9; i++) {
+    	let newDiv = document.createElement("div")
+      newDiv.setAttribute("id", `${i}`)
+      let newDivContent = document.createTextNode(`hello ${i}`)
+      newDiv.appendChild(newDivContent)
+      // newDiv.addEventListener("click", myfunction)
+      board.appendChild(newDiv)
+    }
+
+    let auto = ""
+    for(let i=0; i < 3; i++) {
+    	auto += "auto "
+    }
+    board.setAttribute(
+    "style",
+    `display: grid;grid-template-columns: ${auto};`
+    )
+  }
+
+  constructor(boardSize) {
+    // create board in memory
     let winner = null;
+    this.initializeBoard(boardSize)
   }
 
   play(playerToPlay) {
     // set up the listeners
     console.log('play')
   }
+
+  gameOver() {
+    return false
+  }
+
+  winner() {
+
+  }
 }
 
 class Game {
   constructor() {
-    const player1 = new Player("X", "One");
-    const player2 = new Player("O", "Two");
-    let board = new Board;
-    let lastPlayerToMove = null;
+    this.player1 = new Player("X", "One");
+    this.player2 = new Player("O", "Two");
+    this.board = new Board;
+    this.lastPlayerToMove = null;
   }
 
   playGame() {
     console.log("playing game!!!")
-
-    while (!board.gameOver()) {
-      let playerToPlay = determinePlayerToPlay(lastPlayerToMove);
+    // debugger
+    // while (!this.board.gameOver()) {
+      let playerToPlay = this.determinePlayerToPlay(this.lastPlayerToMove);
       document
-        .getElementById('player-to-play')
-        .innerHTML = `Player ${playerToPlay.turn}`
-      board.play(playerToPlay) // this is where we are right now
-    }
-    if (board.winner) {
-      return board.winner
+        . getElementById("player-to-play")
+        .innerHTML = `Player ${playerToPlay.turn} to play!`
+      //this.board.play(playerToPlay) // this is where we are right now
+    // }
+
+
+    if (this.board.winner()) {
+      return this.board.winner
     } else {
       console.log("it's a tie!")
     }
@@ -48,9 +76,9 @@ class Game {
 
   determinePlayerToPlay(lastPlayerToMove) {
     if(lastPlayerToMove === null || lastPlayerToMove === player2) {
-      return player1
+      return this.player1
     } else {
-      return playerToMove
+      return this.player2
     }
   }
 }
